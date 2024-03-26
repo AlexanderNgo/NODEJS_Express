6e4c4ed52d086470e8a79f322a04fa6feb975339
AlexanderNgo <117934442+AlexanderNgo@users.noreply.github.com>
2024-03-26 12:10:46 +0100

    Add files via upload

--- book-api.js
+++ book-api.js
@@ -10,6 +10,7 @@ let books = [];
 // Route pour récupérer tous les livres
 router.get('/books', (req, res) => {
     res.json(books);
+    console.log("GET");
 });
 
 // Route pour ajouter un nouveau livre
@@ -17,8 +18,12 @@ router.post('/books', (req, res) => {
     const newBook = req.body;
     books.push(newBook);
     res.status(201).json(newBook);
+    //console.log(newBook);
+    //console.log(typeof(newBook)); OBJECT
+    //console.log(books.length); 1
+    console.log(req.body);
 });
 
 // Autres routes pour mettre à jour, récupérer ou supprimer des livres...
 
-
+module.exports = router;
--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const bookRouter = require('./book-api'); // Import le routeur de book-api.js
 
 app.use(express.static(path.join(__dirname,'TD_NODEJS_EXPRESS')));
+app.use(express.json());
+// Utilisation du routeur book-api.js
+app.use('/', bookRouter);
 
 app.get('/',(req,resp)=>{
     //console.log("Hello");
     //resp.send("Hello World");
     resp.sendFile(path.join(__dirname,'index.html'));
+   //resp.sendFile(path.join(__dirname,'index.css'));
 })
+app.get('/index.css', (req, res) => {
+    res.type('text/css');
+    res.sendFile(path.join(__dirname, 'index.css'));
+});
+
 app.listen(port,()=>{
     console.log("Ecoute au port "+port);
 })
