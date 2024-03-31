ecab91a337b2f2ed5524b2163c0bb21cd153d72a
AlexanderNgo <117934442+AlexanderNgo@users.noreply.github.com>
2024-03-31 21:04:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const bookRouter = require('./book-api'); // Import le routeur de book-api.js
+//const bookRouter = require('./book-api'); // Import le routeur de book-api.js
+const bookRouter = require('./book-api'); // Importe à la fois router et books
 
 app.use(express.static(path.join(__dirname,'TD_NODEJS_EXPRESS')));
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 // Utilisation du routeur book-api.js
 app.use('/', bookRouter);
 
@@ -19,6 +21,7 @@ app.get('/index.css', (req, res) => {
     res.type('text/css');
     res.sendFile(path.join(__dirname, 'index.css'));
 });
+
 
 app.listen(port,()=>{
     console.log("Ecoute au port "+port);
